0ca8e937b6ebcbb78d33bd5f544631756d60cde6
ssouzageo <ssouzageovaneo@gmail.com>
2024-12-28 13:12:26 -0300

    Mostrar mensagem preencha os campos.

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         // 2. Criar uma varíavel e recupera um componente de UI <EditText>
         // 3. Recuperar o botão da tela;
         // 4. Colocar ação do botão;
-        // 5. Recuperar o texto dgitado nos edts.
+        // 5. Recuperar o texto dgitado nos edts; e
 
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edtpeso)
@@ -23,13 +24,31 @@ class MainActivity : AppCompatActivity() {
 
         btnCalcular.setOnClickListener {
 
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            val alturaq2 = altura * altura
-            val resultado = peso / alturaq2
+            if (pesoStr == "" || alturaStr == "") {
+                // 6. Mostrar mensagem para o usuário.
 
-            println("Geo ação do botão " + resultado)
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos.",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaq2 = altura * altura
+                val resultado = peso / alturaq2
+
+                println("Geo ação do botão " + resultado)
+            }
+
+
         }
     }
 }
